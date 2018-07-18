0e7b081a3a7364d3b83f81cbc5a4e55353faa38d
michaelbruner <michaelbruner@tamu.edu>
2018-07-18 13:18:46 -0500

    updated perspective with vr and multi

---
+++ Assets/Scripts/DisplayScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		//activate all connected displays 
+		for (int i = 0; i < Display.displays.Length; i++) {
+			Display.displays [i].Activate ();
+		}
+	}
+
+}
